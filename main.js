2737346d71b541cc3bffa10762c68f2f8fd9dde8
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2020-05-14 09:44:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,11 @@ function dispJoke(){
   fetch(' https://sv443.net/jokeapi/v2/joke/Any?type=single')
   .then(res => res.json())
   .then(data =>{
-      var joke = data['joke'],
-        setup = data['setup'],
-        delivery = data['delivery'];
+      var joke = data.joke,
+        setup = data.setup,
+        delivery = data.delivery;
 
         console.log(joke)
-        console.log(setup)
-        console.log(delivery)
 
     //Assigning the line gotten from the API to a P tag 
        display = `<p>${joke}</p>`;
