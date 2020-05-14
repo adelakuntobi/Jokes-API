98ed94fdff729cbbe91d0aacd757722bcf398a41
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2020-05-14 03:05:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
   
 function dispJoke(){
+  //Fetching the data from the API
   fetch(' https://sv443.net/jokeapi/v2/joke/Any?type=single')
   .then(res => res.json())
   .then(data =>{
@@ -11,10 +12,12 @@ function dispJoke(){
         console.log(setup)
         console.log(delivery)
 
+    //Assigning the line gotten from the API to a P tag 
        display = `<p>${joke}</p>`;
+    //Appending the P tap to the div created in the HTML
       document.getElementById('dispContent').innerHTML = display;
   })
 };
 
-
+//Refreshing the Joke every 20 seconds
 setInterval(dispJoke, 20000)
