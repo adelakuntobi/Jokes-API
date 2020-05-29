a627b4bae8a579e1a2a68149017346449e80c35c
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2020-05-29 21:43:23 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
   
 function dispJoke(){
-  //Fetching the data from the API
   fetch(' https://sv443.net/jokeapi/v2/joke/Any?type=single')
   .then(res => res.json())
   .then(data =>{
-      var joke = data.joke,
-        setup = data.setup,
-        delivery = data.delivery;
-
+      var joke = data.joke;
         console.log(joke)
 
-    //Assigning the line gotten from the API to a P tag 
        display = `<p>${joke}</p>`;
-    //Appending the P tap to the div created in the HTML
-      document.getElementById('containerDiv').innerHTML = display;
+      document.getElementById('dispContent').innerHTML = display;
   })
+  .catch(err => console.log(err))
 };
 
 dispJoke();
 
-//Refreshing the Joke every 20 seconds
 setInterval(dispJoke, 20000)
+
